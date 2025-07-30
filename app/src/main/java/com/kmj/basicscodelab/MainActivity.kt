abeda7ee26067b89e495f988872fe58631f02c27
mjkou <miaojuan.kou@rea-group.com>
2025-07-30 19:01:38 +0800

    Remove unused import

--- app/src/main/java/com/kmj/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/kmj/basicscodelab/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
